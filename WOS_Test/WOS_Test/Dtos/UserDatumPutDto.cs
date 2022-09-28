fb77e2dd282e45c18cfe02fbc821a113405ae020
show8301 <b27873027@gmail.com>
2022-09-28 17:21:12 +0800

    Add Password Validation Attribute

--- WOS_Test/WOS_Test/Dtos/UserDatumPutDto.cs
+++ WOS_Test/WOS_Test/Dtos/UserDatumPutDto.cs
@@ -10,6 +10,7 @@ namespace WOS_Test.Dtos
         [Username]
         public string Username { get; set; } = null!;
 
+        [Password]
         [RegularExpression(@"^[a-zA-Z][a-zA-Z0-9_]{6,20}$", ErrorMessage ="開頭必須為英文字母，除底線以外不接受其他特殊符號")]
         public string Password { get; set; } = null!;
     }
---
+++ WOS_Test/WOS_Test/ValidationAttributes/PasswordAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WOS_Test.Dtos;
+using WOS_Test.Models;
+
+namespace WOS_Test.ValidationAttributes
+{
+    public class PasswordAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var pw = (string)value;
+            string name="";
+
+            if(validationContext.ObjectInstance.GetType() == typeof(UserDatumPostDto))
+            {
+                var user = (UserDatumPostDto)validationContext.ObjectInstance;
+                name = user.Username;
+            }
+            else if(validationContext.ObjectInstance.GetType() == typeof(UserDatumPutDto))
+            {
+                var user = (UserDatumPutDto)validationContext.ObjectInstance;
+                name = user.Username;
+            }
+ 
+            if (name == pw)
+            {
+                return new ValidationResult("密碼不得與使用者名稱相同");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
