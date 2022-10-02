30aaf1adb382b89865f0449d3affd7c9f1a9b88c
show8301 <b27873027@gmail.com>
2022-10-02 19:33:56 +0800

    Create FileUploadController

---
+++ WOS_Test/WOS_Test/Controllers/FileUploadController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WOS_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        
+        private readonly IWebHostEnvironment _env;
+
+        public FileUploadController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        public void Post(List<IFormFile> files)
+        {
+            string root = _env.ContentRootPath + @"\wwwroot\";
+
+            foreach(var file in files)
+            {
+                string fileName = file.FileName;
+
+                using (var stream = System.IO.File.Create(root + fileName))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+        }
+    }
+}
